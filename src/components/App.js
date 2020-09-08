a8b8f20b4b41adac744ff5155bdbbe2d6bf94d11
Lochlan Odlum <lodlum5@gmail.com>
2020-09-08 22:03:08 +0100

    updated header anchor links so only goes back to arcade.lochlan.cc

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ const App = () => {
             <div className="main-header">
               <span className="name">
                 <a className="nostyle header-link" href="http://arcade.lochlan.cc">Arcade ||</a>
-                <a className="nostyle header-link" href="http://lochlan.cc"> Lochlan.cc</a>
+                <a className="nostyle header-link" href="http://arcade.lochlan.cc"> Lochlan.cc</a>
               </span>
             </div>
 
--- src/components/SmallHeader.js
+++ src/components/SmallHeader.js
@@ -7,7 +7,7 @@ const SmallHeader = () => {
     <div className="ttt-header">
               <span className="ttt-name">
                 <a className="nostyle header-link" href="http://arcade.lochlan.cc">Arcade ||</a>
-                <a className="nostyle header-link" href="http://lochlan.cc"> Lochlan.cc</a>
+                <a className="nostyle header-link" href="http://arcade.lochlan.cc"> Lochlan.cc</a>
               </span>
     </div>
   );
