6c13425b0096bb7c3b915d26e45a66c411c5f6f2
BuildTools <unconfigured@null.spigotmc.org>
2020-09-22 12:07:05 +0100

    updated href root for header so will go to root instead of hard coded arcade.lochlan.cc

--- src/components/SmallHeader.js
+++ src/components/SmallHeader.js
@@ -2,12 +2,12 @@ import React from 'react';
 import '../styling/TTT/TicTacHeader.css';
 
 const SmallHeader = () => {
-
+    
   return (
     <div className="ttt-header">
               <span className="ttt-name">
-                <a className="nostyle header-link" href="http://arcade.lochlan.cc">Arcade ||</a>
-                <a className="nostyle header-link" href="http://arcade.lochlan.cc"> Lochlan.cc</a>
+                <a className="nostyle header-link" href='/'>Arcade ||</a>
+                <a className="nostyle header-link" href='/'> Lochlan.cc</a>
               </span>
     </div>
   );
